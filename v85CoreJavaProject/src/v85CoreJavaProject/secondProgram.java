86ee44f00d2674780887ec41cb03e7c02432a63e
Adeeb <adeeb5.sadiq7@gmail.com>
2024-03-14 10:13:40 +0530

    adding second program

---
+++ v85CoreJavaProject/src/v85CoreJavaProject/secondProgram.java
@@ -0,0 +1,10 @@
+package v85CoreJavaProject;
+
+public class secondProgram {
+	public static void main(String args[]) {
+		short a = 10;
+		short b = 20;
+		int c = a + b;
+		System.out.println("The sum of two numbers is: " + c);
+	}
+}
