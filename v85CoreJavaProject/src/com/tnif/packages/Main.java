7061cb3a23e150bcbfe3e1acfee25518c96048a0
Adeeb <adeeb5.sadiq7@gmail.com>
2024-03-21 19:35:33 +0530

    encapsulation

--- v85CoreJavaProject/src/com/tnif/packages/Main.java
+++ v85CoreJavaProject/src/com/tnif/packages/Main.java
@@ -5,14 +5,13 @@ import basics.carclass;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Student s = new Student();
-		
-		s.display();
-		
 		carclass obj = new carclass("bmw","m5","black",600,250000);
 		obj.displayBrand();
-
+		
+		Student s = new Student();
+		s.setStudentName("zoro");
+		
+		System.out.println(s);
 	}
 
 }
