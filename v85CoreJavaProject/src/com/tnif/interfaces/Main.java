be053b81757d67a59cac4605421ba82205c5a134
Adeeb <adeeb5.sadiq7@gmail.com>
2024-04-08 20:54:56 +0530

    interfaces in java

---
+++ v85CoreJavaProject/src/com/tnif/interfaces/Main.java
@@ -0,0 +1,29 @@
+package com.tnif.interfaces;
+
+public class Main implements Math{
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float mul(float a, float b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float div(float a, float b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ v85CoreJavaProject/src/com/tnif/interfaces/Math.java
@@ -0,0 +1,8 @@
+package com.tnif.interfaces;
+
+public interface Math {
+	public int add(int a, int b);
+	public int sub(int a, int b);
+	public float mul(float a, float b);
+	public float div(float a, float b);
+}
