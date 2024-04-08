69a0b403a158b3c49612b16662804805aac435f0
Adeeb <adeeb5.sadiq7@gmail.com>
2024-04-08 20:36:15 +0530

    method overloading

---
+++ v85CoreJavaProject/src/com/tnif/overloading/Main.java
@@ -0,0 +1,12 @@
+package com.tnif.overloading;
+
+public class Main {
+	public static void main(String[] args) {
+		MethodOverloading mo = new MethodOverloading();
+		int a = mo.sum(20, 30);
+		int b = mo.sum(1, 1, 1);
+		
+		System.out.println("Example for method overloading.");
+		System.out.println(a + " " + b);
+	}
+}
---
+++ v85CoreJavaProject/src/com/tnif/overloading/MethodOverloading.java
@@ -0,0 +1,12 @@
+package com.tnif.overloading;
+
+public class MethodOverloading {
+	
+	public int sum(int a, int b) {
+		return a + b;
+	}
+	
+	public int sum(int a, int b, int c) {
+		return a + b + c;
+	}
+}
