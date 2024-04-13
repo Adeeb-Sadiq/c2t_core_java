1d9e5cc35ef4d834e83984c4fcb1f2e88fb91968
Adeeb <adeeb5.sadiq7@gmail.com>
2024-04-13 15:18:27 +0530

    commit

--- v85CoreJavaProject/src/com/tnif/exceptionHandling/CheckAge.java
+++
@@ -1,5 +0,0 @@
-package com.tnif.exceptionHandling;
-
-public class CheckAge {
-
-}
--- v85CoreJavaProject/src/com/tnif/exceptionHandling/VoterValidation.java
+++ v85CoreJavaProject/src/com/tnif/exceptionHandling/VoterValidation.java
@@ -1,5 +1,33 @@
 package com.tnif.exceptionHandling;
 
-public class VoterValidation {
+import java.util.Scanner;
 
+public class VoterValidation {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String name = "SomeName";
+		int age = 11;
+		
+//		System.out.println("Enter your full name: ");
+//		name = sc.nextLine();
+		
+//		System.out.println("Enter your age: ");
+//		age = sc.nextInt();
+		
+		
+		try {
+			checkage(name, age);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+	}
+	
+	public static void checkage(String name, int age) throws Exception {
+		if(age < 18) {
+			throw new Exception("age under 18");
+		}
+		System.out.println(name+ " "+ age);
+	}
 }
